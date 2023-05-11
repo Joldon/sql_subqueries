7dc9126dd818ec88bdc9dc952542b30c5aeaca59
joldon <79482239+Joldon@users.noreply.github.com>
2023-05-11 20:28:51 +0200

    made additional queries on movies and directors

--- imdb_movies.sql
+++ imdb_movies.sql
@@ -48,21 +48,56 @@ ORDER BY count DESC; -- Eurovision Song Contest, The
 
 /* Understanding the database
 
-Are there movies with multiple directors?
-What is the movie with the most directors? Why do you think it has so many?
-On average, how many actors are listed by movie?
+
+
+
 Are there movies with more than one “genre”?
 */
+
+
+
+-- Are there movies with multiple directors?
+SELECT m.name AS movie_name, COUNT(DISTINCT d.id) AS director_count
+FROM movies AS m
+INNER JOIN movies_directors AS md ON m.id = md.movie_id
+INNER JOIN directors AS d ON md.director_id = d.id
+GROUP BY m.name
+HAVING director_count > 1;
+
+-- What is the movie with the most directors? Why do you think it has so many?
+-- this one aggregates all directors by the movies with the same name but produced in different years
+-- basically thsi sums up directors from different movies but have the same name
+SELECT m.name AS movie_name, COUNT(DISTINCT d.id) AS director_count
+FROM movies AS m
+INNER JOIN movies_directors AS md ON m.id = md.movie_id
+INNER JOIN directors AS d ON md.director_id = d.id
+GROUP BY m.name
+ORDER BY director_count DESC;
+
+-- this code separates movies with the same name but with different years treating them as separate entities
+-- but does sort them by number of directors
+SELECT m.name AS movie_name, m.year, COUNT(DISTINCT d.id) AS director_count
+FROM movies AS m
+INNER JOIN movies_directors AS md ON m.id = md.movie_id
+INNER JOIN directors AS d ON md.director_id = d.id
+GROUP BY m.name, m.year
+HAVING director_count > 1;
+
+-- this code sorts the list by number of directors but there is another error because teh least number of directors is 9
+-- which is not realistic. Check what causes this bug
+SELECT m.name AS movie_name, m.year, COUNT(DISTINCT d.id) AS director_count
+FROM movies AS m
+INNER JOIN movies_directors AS md ON m.id = md.movie_id
+INNER JOIN directors AS d ON md.director_id = d.id
+GROUP BY m.name, m.year
+HAVING director_count > 1
+ORDER BY director_count DESC;
 
 select * from movies_directors;
 select * from directors;
 select * from movies;
 select * from directors_genres;
-SELECT 
-
-
-
-
+-- On average, how many actors are listed by movie?
 
 
 
