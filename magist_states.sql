7cff07185429b272e4f74ab0e4359162e042d5d8
joldon <79482239+Joldon@users.noreply.github.com>
2023-05-16 11:10:55 +0200

    magist_state file

---
+++ magist_states.sql
@@ -0,0 +1,13 @@
+USE magist;
+
+select * from geo;
+select * from sellers;
+select * from states_en;
+
+/* Analyze customer reviews
+Find the average review score by state of the customer.
+Do reviews containing positive words have a better score? Some Portuguese positive words are: “bom”, “otimo”, “gostei”, “recomendo” and “excelente”.
+Considering only states having at least 30 reviews containing these words, what is the state with the highest score?
+What is the state where there is a greater score change between all reviews and reviews containing positive words? 
+*/
+
